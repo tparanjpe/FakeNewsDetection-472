e9e7317b0a85c7b559f4aa6f311d02cea378cd3a
Stephanie Lee <stephanie452780@gmail.com>
2021-11-17 15:49:45 -0700

    language translation code

---
+++ testingtranslation.py
@@ -0,0 +1,39 @@
+
+import translators as ts
+
+for x in collected_URLs:
+   driver.get(x)
+   
+   content = driver.page_source
+   soup = BeautifulSoup(content, features="html.parser")
+
+   translatedSentences = []
+   translatedArticles = []
+
+   for x in titles:
+
+      print("x length: ", len(x))
+      if len(x) > 100:
+         split_strings = []
+         temp = ''
+         for index in range(0, len(x)):
+            if x[index] != '.' and x[index] != '!':
+               temp += x[index]
+               # print(temp)
+            else:
+               split_strings.append(temp)
+               temp = ''
+         print(split_strings)
+         for y in split_strings:
+            print('orginal text: ',y)
+            result = (ts.google(y))
+            print('translated text: ', result)
+            translatedSentences.append(result)
+      else:
+         print('orginal text: ',x)
+         result = (ts.google(x))
+         print('translated text: ', result)
+         translatedSentences.append(result)
+      
+   print("translated sentences: ", translatedSentences)
+   
